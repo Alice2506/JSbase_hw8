5c81aa7b786dc24d9439f6c681c8277db2fa65bb
Alice2506 <ralko84@mail.ru>
2023-09-17 20:02:02 +0300

    added task 2

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function getCube(number) {
 
 console.log(getCube(2));
 console.log(getCube(3));
+
+/*
+Задание 2
+Пользователь вводит с клавиатуры число, если ввёл текст, необходимо вывести что значение задано неверно
+Создать фукнцию, которая высчитывает 13% от данного числа и выводит в консоль текст 
+"Размер заработной платы за вычетом налогов равен значение"
+*/
+
+let userNumber = Number(prompt('Введите число'));
+
+if (isNaN(userNumber)) {
+    console.log('Введите данные в числовом формате');
+}
+
+function getSalary() {
+    let salary = userNumber - userNumber * 0.13;
+    return salary;
+}
+
+console.log(getSalary());
